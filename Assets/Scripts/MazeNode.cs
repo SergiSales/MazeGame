81d571b77f67ebd6e2adb4516224da3a255e4b9d
SergiSales <al416089@uji.es>
2025-05-31 14:07:09 +0200

    Initial commit

---
+++ Assets/Scripts/MazeNode.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+enum NodeState
+{
+    Available,
+    Current,
+    Completed,
+    PlayerVisited,
+    PlayerCurrent,
+    PlayerHidden
+    
+}
+public class MazeNode : MonoBehaviour
+{
+    public GameObject[] walls;
+    public MeshRenderer floor;
+
+
+    public void RemoveWall(int wallIndex)
+    {
+        walls[wallIndex].SetActive(false); // walls[wallIndex].destroy();
+    }
+    public void SetState(int state)
+    {
+        switch (state)
+        {
+            case (int)NodeState.Available:
+                floor.material.color = Color.white;
+                break;
+            case (int)NodeState.Current:
+                floor.material.color = Color.yellow;
+                break;
+            case (int)NodeState.Completed:
+                floor.material.color = Color.blue;
+                break;
+        }
+    }
+}
